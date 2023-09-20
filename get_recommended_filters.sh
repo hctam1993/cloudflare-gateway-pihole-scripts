3397e620383c0c670f8ea128810e78d2fe70a72d
hctam1993 <103570445+hctam1993@users.noreply.github.com>
2023-09-20 14:57:26 +0700

    Update get_recommended_filters.sh
    add blocklist

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 node download_lists.js blocklist
+https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts
+https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+https://raw.githubusercontent.com/hagezi/dns-blocklists/main/hosts/multi.txt
+https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
